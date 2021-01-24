db94009a4cd10fa7df94d30c41021d113f2a5f0d
MeldaKose <59098685+MeldaKose@users.noreply.github.com>
2021-01-24 19:33:26 +0300

    Proje dosyası ekle.

---
+++ DictionaryProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DictionaryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+            myDictionary.Add(123,"Melda");
+            myDictionary.Add(456, "Serpil");
+
+        }
+    }
+}
